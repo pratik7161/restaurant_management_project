312fe11d370260eea2c00774ceecde2c579fad9a
pratik7161 <pratik2552002@gmail.com>
2025-08-04 22:01:33 +0530

    17884

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import *
+from .models import Menu,Order, OrderItem
+
 
 
 # Custom Admins
@@ -9,3 +10,17 @@ class ItemAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Item,ItemAdmin)
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1 # number of extra empty items shown by default
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id','customer','total_amount','status','created_at')
+    list_filter = ('status',)
+    search_fields = ('name',)
+    
+
