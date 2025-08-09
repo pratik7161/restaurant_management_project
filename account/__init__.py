26ad200ddac5f8362c110c5b38d4df7bd014f3cb
pratik7161 <pratik2552002@gmail.com>
2025-08-09 22:27:06 +0530

    28355

---
+++ account/__init__.py
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <title>Page Not Found - 404</title>
+     <style>
+       body{
+             background-color: #f8f9fa;
+             font-family:Arial, sans-serif;
+             text-align: center;
+             padding: 50px;
+             color: #333;
+    }
+
+    h1{
+       font-size:80px;
+       margin-bottom:20px;
+       color: #ff4c4c;
+
+
+    }
+    h2{
+       font-size:24px;
+       margin-bottom: 20 px;
+    }
+    a{
+
+        text-decoration: none;
+        color: white;
+        background-color: #0043333333;
+        padding: 10px 20px;
+
+    }
+
+    a:hover{
+
+        background-color: #dfd;
+
+    }
+    </style>
+</head>
+<body>
+
+ <h1>404</h1>
+ <h2>Ooops! The Oage You Are Looking For Does not exist.</h2>
+ <p>it might be removed</p>
+ </body>
+ </html>
