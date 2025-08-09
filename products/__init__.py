29035302588169bf8011bf2cc6e3e806827fc36f
pratik7161 <pratik2552002@gmail.com>
2025-08-09 22:10:37 +0530

    28354

---
+++ products/__init__.py
@@ -0,0 +1,56 @@
+body{
+    font-family: 'Segoe UI', Tahoma, Geneva,Verdana,sans-serif;
+    background-color: #f8f9a;
+    margin: 0;
+    padding: 0;
+}
+
+
+header {
+
+    background-color: #ff6b6b;
+    color: white;
+    padding: 20px;
+    text-align: center;
+
+}
+
+h1 {
+
+    font-size: 2.5rem;
+    margin-bottom: 10px;
+
+
+}
+p{
+
+    font-size:1.2rem;
+    color:#555;
+
+}
+
+
+container{
+
+    max-width: 900px;
+    margin: 40px;
+    text-align: center;
+
+}
+
+
+.btn {
+
+    background-color: #4cafef;
+    color: white;
+    padding: 12px 25px;
+    text-decoration: none;
+    boder-radius: 5px;
+
+}
+
+.btn:hover {
+
+    background-color: #2b82d6;
+    
+}
