f8670c51d23d28f000b43a7f627b2a140c7fdf6b
pratik7161 <pratik2552002@gmail.com>
2025-08-30 22:29:54 +0530

    40343

--- form.py
+++ form.py
@@ -5,3 +5,25 @@ clss ContactForm(forms.ModelForm):
     model = ContactSubmission
     fields = ['name', 'email']
     
+from django.db import models
+class MenuItem(models.Model):
+    name = models.CharField(max_length= 100)
+    description = model.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.Image.Field(upload_to='uploads/')
+    def __str__(self):
+        return self.name
+
+
+# configure settinf.py
+MEDIA_URL = '/media/'
+MEDIA_PORT = BASE_DIR / 'media'
+# project/urls.py
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    #  ... your app urls
+]
+if settings.DEBUG:
+    urlpattern += static(setting.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
