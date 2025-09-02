1cba078d853f98bb3c9284bf8d679334c7e3f454
pratik7161 <pratik2552002@gmail.com>
2025-09-02 18:17:23 +0530

    40341

--- models.py
+++ models.py
@@ -1,25 +1,8 @@
-from django.db import models
-
-class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    submittted_at = models.DateTimeField(auto_now_add=True)
-
-class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='menu_images/', blank=True, null= True)
-    
+form django.db import models
+class Restaurant(models.Model):
+    name = models.CharFeild(max_length=200)
+    address = models.TextField()
     def __str__(self):
-        return f"{self.name} - {self.email}"
+        return self.name
 
 
-class RestaurantLocation(models.Model):
-    address = models.CharField(max_length=255)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    zip_code = models.CharField(max_length=20)
-    def __str__(self):
-        return f"{self.address}, {self.city}, {self.state} - {self.zip_code}"
-        
