46e7d40c0430d7fc5bf646efa64bc38d3f81bd12
pratik7161 <pratik2552002@gmail.com>
2025-09-06 16:46:30 +0530

    43105

--- views.py
+++ views.py
@@ -7,3 +7,20 @@ def homepage(request):
         "phone": settings.RESTAURANT_PHONE
     }
     return render(request,"home.html", context)
+
+    +
+from django.shortcuts import render
+from .models import Restaurant, MenuItem
+def homepage(request):
+    restaurant = Restaurant.objects.first()
+    items = MenuItem.objects.all()
+
+cart = request.session.get("cart",{})
+# count total items in cart
+cart_count = sum(cart.values())
+return render(request, "homepage.html",{
+    "restaurant": restaurant,
+    "items": items,
+    "cart_count": cart_count,
+    
+})
