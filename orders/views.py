bcab0e6e4a0bfb3f18f88cf1e94bc586a6c086ac
pratik7161 <pratik2552002@gmail.com>
2025-09-10 22:42:48 +0530

    43609

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def  faq(request):
+    faqs = [
+        {"question":"what are your opening hours?", "answer": "we are open daily" },
+        {"question":"do you offer home delivery?", "answer": "yes , we provide it 24/7 "},
+        {"question":"do you accept online payments?", "answer": "yes , but if customer doesn't have cash money"},
+        {"question": "do you offer vegetarian options?", "answer":"yes , offcourse"}
+        ]
+        return render(request,"faq.html",{"faqs": faqs})
