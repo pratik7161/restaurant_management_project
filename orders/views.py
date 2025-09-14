7fa55e5502743423d0d2f63e99d4b1a8355f5ba2
pratik7161 <pratik2552002@gmail.com>
2025-09-14 23:44:54 +0530

    44612

--- orders/views.py
+++ orders/views.py
@@ -13,4 +13,13 @@ def format_datetime(value, format_string="%A,%B,%C,%Y,%I"):
     if not value:
         value = timezone.now()
     return value.strftime(format_string)
-    
+
++
+from django.shortcuts import render
+def privacy_policy(request):
+    return render(request, "privacy_policy.html")
+
+{% block content %}
+<div class="container" style="max-width:800px; margin:50px auto; padding:20px; background:#f9f9f9; border-radius:10px;"
+     <h2 style="text-align: center; margin-bottom:20px;">Privacy Policy</h2>
+     <p>We value your Privacy. This Privacy policy outline how our restaurant websites collect,uses, and protects your information.</p>    
